884416d598cb833dfd14cc94ad4104cf58cc11fa
Sreehitha2003 <87296340+Sreehitha2003@users.noreply.github.com>
2022-02-16 10:19:52 +0530

    Update LeaveRequests.jsx

--- frontend/src/components/Leave Request/Leave Request/LeaveRequests.jsx
+++ frontend/src/components/Leave Request/Leave Request/LeaveRequests.jsx
@@ -1,51 +1,28 @@
-import React, { useMemo } from "react";
+import React, { useEffect, useMemo } from "react";
+import { useState } from "react";
 import { useTable, useGlobalFilter, usePagination } from "react-table";
 import { COLUMNS } from "./columns";
 import GlobalFilter from "./GlobalFilter";
 import styles from "./LeaveRequests.module.css";
 import Navbar from "../../Navbar/Navbar";
 import Card from "../../UI/Card";
-import { Link } from "react-router-dom";
-
-const Data = [
-  {
-    id: 1,
-    Employee_Name: "Darin Farrand",
-    Leave_Date: "10/9/2016",
-    Days: 7,
-    Purpose:
-      "Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.",
-  },
-  {
-    id: 2,
-    Employee_Name: "Doralynne De Paepe",
-    Leave_Date: "7/18/2017",
-    Days: 1,
-    Purpose: "Fusce consequat. Nulla nisl. Nunc nisl.",
-  },
-  {
-    id: 3,
-    Employee_Name: "Rici Gobat",
-    Leave_Date: "6/9/2017",
-    Days: 8,
-    Purpose: "",
-  },
-  {
-    id: 4,
-    Employee_Name: "Carmela Rodie",
-    Leave_Date: "11/21/2016",
-    Days: 10,
-    Purpose: "",
-  },
-];
+import NewLeaveRequest from "./NewLeaveRequest"
 
 const LeaveRequests = () => {
+  const [leaverequests, setLeaverequests] = useState([]);
+  const [newLeaveRequest, setnewLeaveRequest] = useState(false);
+
+  useEffect(() => {
+    sendRequest();
+  }, []);
+
   const lid = 1;
   const confirmHandler = () => {
     window.location.href = `/${lid}/leaveConfirmation`;
   };
   const columns = useMemo(() => COLUMNS, []);
-  const data = useMemo(() => Data, []);
+  const data = useMemo(() => leaverequests, [leaverequests]);
+
 
   const {
     getTableProps,
@@ -72,16 +49,50 @@ const LeaveRequests = () => {
     usePagination
   );
 
+  const sendRequest = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/leaverequests/");
+      const responseData = await response.json();
+      if (!response.ok) {
+        throw new Error(responseData.message);
+      }
+      setLeaverequests(responseData.leaverequests);
+      console.log(leaverequests)
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   const { globalFilter, pageIndex, pageSize } = state;
+
+  const closeHandler = () => {
+    setnewLeaveRequest(false);
+  };
+
+  const submitHandler = () => {
+    setnewLeaveRequest(false);
+  };
 
   return (
     <>
       <Navbar />
       <Card>
         <div className={styles.LeaveRequests}>
+        <div className={styles.Leaverequests}>
           <div className={styles.heading}>
             <h1>Leave Requests</h1>
           </div>
+          <button
+          className={`btn ${styles.newLeaveRequest}`}
+          onClick={() => {
+            setnewLeaveRequest(true);
+          }}
+        >
+          Add New
+        </button>
+        {newLeaveRequest && (
+            <NewLeaveRequest onAdd={submitHandler} onClose={closeHandler} />
+          )}
           <div className={styles.search}>
             <GlobalFilter filter={globalFilter} setFilter={setGlobalFilter} />
           </div>
@@ -113,9 +124,6 @@ const LeaveRequests = () => {
                     })}
                     <td>
                       <button onClick={confirmHandler}>Confirm</button>
-                      {/* <Link to={`/${lid}/leaveConfirmation`}>
-                        <button>Confiram</button>
-                      </Link> */}
                     </td>
                   </tr>
                 );
@@ -172,7 +180,8 @@ const LeaveRequests = () => {
             </button>
           </div>
         </div>
-      </Card>
+        </div>
+        </Card>
     </>
   );
 };
