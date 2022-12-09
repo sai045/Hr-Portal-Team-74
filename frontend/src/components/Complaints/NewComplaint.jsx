7e0f3111e8e8880a7d0d803c6ef0ee37ec1205ea
Aalasyam Sai Varshith <85741790+sai045@users.noreply.github.com>
2022-12-09 11:28:39 +0530

    Update NewComplaint.jsx

--- frontend/src/components/Complaints/NewComplaint.jsx
+++ frontend/src/components/Complaints/NewComplaint.jsx
@@ -6,7 +6,7 @@ const NewComplaint = (props) => {
   const sendRequest = async () => {
     try {
       const response = await fetch(
-        "https://hr-portal-team-74.vercel.app/complaints/",
+        "https://hr-portal-team-74.vercel.app/api/complaints/",
         {
           method: "POST",
           headers: {
