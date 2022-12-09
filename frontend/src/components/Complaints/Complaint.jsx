850a741e0a88f8dc3307486f9fc7d168da79aeb8
Aalasyam Sai Varshith <85741790+sai045@users.noreply.github.com>
2022-12-09 11:27:36 +0530

    Update Complaint.jsx

--- frontend/src/components/Complaints/Complaint.jsx
+++ frontend/src/components/Complaints/Complaint.jsx
@@ -6,7 +6,7 @@ const Complaint = (props) => {
   const deleteHandler = async () => {
     try {
       const response = await fetch(
-        `https://localhost:5000/api/complaints/${props.id}`,
+        `https://hr-portal-team-74.vercel.app/complaints/${props.id}`,
         {
           method: "DELETE",
           headers: {
