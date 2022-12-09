314fe8f4574af17512eb7262786d7e143c0ce4e0
Aalasyam Sai Varshith <85741790+sai045@users.noreply.github.com>
2022-12-09 11:44:18 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -12,8 +12,7 @@ const Navbar = () => {
   // SetId(id);
   const sendRequest = async () => {
     try {
-      const response = await fetch(
-      );
+      const response = await fetch("https://hr-portal-team-74.vercel.app/${Id}");
       const responseData = await response.json();
       console.log(responseData)
       if (!response.ok) {
