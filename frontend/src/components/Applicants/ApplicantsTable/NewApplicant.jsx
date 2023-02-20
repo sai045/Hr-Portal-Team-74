64dba210deced35b86954d948490b304f1ddece6
Tejasreeee <85746815+tejasri415@users.noreply.github.com>
2023-02-20 18:45:44 +0530

    Update NewApplicant.jsx

--- frontend/src/components/Applicants/ApplicantsTable/NewApplicant.jsx
+++ frontend/src/components/Applicants/ApplicantsTable/NewApplicant.jsx
@@ -6,7 +6,7 @@ const NewComplaint = (props) => {
   const sendRequest = async () => {
     try {
       const response = await fetch(
-        "https://sai045-hr-portal-backend.onrender.com//api/applicant/",
+        "https://sai045-hr-portal-backend.onrender.com/api/applicant/",
         {
           method: "POST",
           headers: {
