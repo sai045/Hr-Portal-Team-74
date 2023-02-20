be7f1448e4732c53a90e5e21c8adc087f04aaf72
Tejasreeee <85746815+tejasri415@users.noreply.github.com>
2023-02-20 18:54:23 +0530

    Update LeaveRequests.jsx

--- frontend/src/components/Leave Request/Leave Request/LeaveRequests.jsx
+++ frontend/src/components/Leave Request/Leave Request/LeaveRequests.jsx
@@ -20,7 +20,7 @@ const LeaveRequests = (props) => {
 
   const sendRequest = async () => {
     try {
-      const response = await fetch("https://hr-portal-team-74.vercel.app/api/leaverequests/");
+      const response = await fetch("https://sai045-hr-portal-backend.onrender.com/api/leaverequests/");
       const responseData = await response.json();
       if (!response.ok) {
         throw new Error(responseData.message);
