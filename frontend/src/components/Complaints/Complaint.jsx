3292bd32a6e23fc07281602532bc767270814a6e
Tejasreeee <85746815+tejasri415@users.noreply.github.com>
2023-02-20 18:46:42 +0530

    Update Complaint.jsx

--- frontend/src/components/Complaints/Complaint.jsx
+++ frontend/src/components/Complaints/Complaint.jsx
@@ -6,7 +6,7 @@ const Complaint = (props) => {
   const deleteHandler = async () => {
     try {
       const response = await fetch(
-        `https://hr-portal-team-74.vercel.app/api/complaints/${props.id}`,
+        `https://sai045-hr-portal-backend.onrender.com/api/complaints/${props.id}`,
         {
           method: "DELETE",
           headers: {
