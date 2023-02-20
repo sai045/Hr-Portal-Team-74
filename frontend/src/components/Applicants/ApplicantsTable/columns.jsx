9c679ac395afab2579ad03d46db902458ec0fc96
Tejasreeee <85746815+tejasri415@users.noreply.github.com>
2023-02-20 18:44:55 +0530

    Update columns.jsx

--- frontend/src/components/Applicants/ApplicantsTable/columns.jsx
+++ frontend/src/components/Applicants/ApplicantsTable/columns.jsx
@@ -28,7 +28,7 @@ export const COLUMNS = [
           const id = href_elements[3];
           const aid = cell.row.values._id;
           window.location.assign(
-            `https://hr-portal-team-74.vercel.app/${id}/applicant/${aid}`
+            `https://hr-portal-team-74-y1r7.vercel.app/${id}/applicant/${aid}`
           );
         }}
       >
